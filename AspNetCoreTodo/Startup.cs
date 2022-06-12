9b2a9df422a42ba8c24966e133a0a613f5aeb674
ilnarik <i1narik@outlook.com>
2022-06-12 22:00:32 +0300

    добавлен интерфейс репозитория

--- AspNetCoreTodo/Startup.cs
+++ AspNetCoreTodo/Startup.cs
@@ -1,4 +1,5 @@
 using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,7 @@ namespace AspNetCoreTodo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
