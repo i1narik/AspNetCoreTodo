2301bd2d844a81376cdd3cad34ac54b70d6b0764
ilnarik <i1narik@outlook.com>
2022-06-12 21:59:37 +0300

    Добавлены controller, model и viewmodel для элемента Todo

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            var model = new TodoViewModel
+            {
+                Items = items
+            };
+            return View(model);
+        }
+    }
+}
